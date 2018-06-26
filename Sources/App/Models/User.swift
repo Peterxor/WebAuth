1916676e2d8ddbf2ef2458d731524b3dad542c12
Peterxor <peter55605@gmail.com>
2018-06-26 11:43:32 +0800

    User Model: extension PasswordAuthenticatable and SessionAuthenticatacle

--- Sources/App/Models/User.swift
+++ Sources/App/Models/User.swift
@@ -1,15 +1,28 @@
 import Vapor
 import FluentSQLite
+import Authentication
 
 final class User: SQLiteModel{
     var id: Int?
-    var username: String
+    var useremail: String
+    var password: String
     
-    init(id: Int? = nil, username: String) {
+    init(id: Int? = nil, useremail: String, password: String) {
         self.id = id
-        self.username = username
+        self.useremail = useremail
+        self.password = password
     }
 }
 
 extension User: Migration{}
 extension User: Content{}
+extension User: PasswordAuthenticatable{
+    static var usernameKey: WritableKeyPath<User, String> {
+        return \User.useremail
+    }
+    static var passwordKey: WritableKeyPath<User, String> {
+        return \User.password
+    }
+}
+
+extension User: SessionAuthenticatable{}
