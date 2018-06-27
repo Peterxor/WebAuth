02e7dcd618f90af68e1970e5efc2588423cca71a
Peterxor <peter55605@gmail.com>
2018-06-27 09:42:48 +0800

    UserController: add a method register

--- Sources/App/Controllers/UserController.swift
+++ Sources/App/Controllers/UserController.swift
@@ -1,8 +1,26 @@
 import Vapor
-
+import FluentSQLite
+import Crypto
 
 final class UserController{
     func renderRegister(req: Request) throws -> Future<View>{
         return try req.view().render("register")
     }
+    
+    func register(req: Request) throws -> Future<Response>{
+        return try req.content.decode(User.self).flatMap({ user in
+            return User.query(on: req).filter(\User.email == user.email).first().flatMap({ result in
+                if let _ = result{
+                    return Future.map(on: req, { () in
+                        return req.redirect(to: "/register")
+                    })
+                }
+                user.password = try BCryptDigest().hash(user.password)
+                return user.save(on: req).map({ (_) in
+                    return req.redirect(to: "/login")
+                })
+            })
+        })
+    }
+    
 }
