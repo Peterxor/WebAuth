17af6ae15bdd785201ae60f9d0fcf564c22cc102
Peterxor <peter55605@gmail.com>
2018-06-27 09:47:07 +0800

    route.swift: add a POST route 'register'

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,4 +3,5 @@ import Vapor
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     router.get("register", use: UserController().renderRegister)
+    router.post("register", use: UserController().register)
 }
